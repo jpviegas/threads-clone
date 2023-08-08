bea5d357f68a98081bce3745efd3c47b0e78bc21
jpviegas <jp.viegas@hotmail.com>
2023-08-08 10:22:47 -0300

    feat: Onboarding page

---
+++ lib/validations/user.ts
@@ -0,0 +1,17 @@
+import * as z from 'zod';
+
+export const UserValidation = z.object({
+  profile_photo: z.string().url().nonempty(),
+  name: z
+    .string()
+    .min(3, { message: 'NAME MUST BE MORE THAN 3 CHARACTERS' })
+    .max(30),
+  username: z
+    .string()
+    .min(3, { message: 'USERNAME MUST BE MORE THAN 3 CHARACTERS' })
+    .max(30),
+  bio: z
+    .string()
+    .min(3, { message: 'BIO MUST BE MORE THAN 3 CHARACTERS' })
+    .max(1000),
+});
